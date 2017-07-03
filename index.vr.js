c80373a6061c9aff651b4fd0f8181f15b9ddb24d
Vasiliy Shubko <flit77@gmail.com>
2017-07-04 00:40:37 +0200

    soundshape component added to main view

--- index.vr.js
+++ index.vr.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Forest from './components/Forest';
+import SoundShape from './components/SoundShape';
 import {
   AppRegistry,
   asset,
   Pano,
-  Text,
+  SpotLight,
   View,
+  Cylinder,
 } from 'react-vr';
 
 export default class musical_exp_react_vr_pusher extends React.Component {
@@ -14,6 +16,20 @@ export default class musical_exp_react_vr_pusher extends React.Component {
       <View>
         <Pano source={asset('images/background.jpg')} />
         <Forest trees={100} perimeter={15} colors={['#016549', '#87b926', '#b1c96b']} />
+        <SpotLight intensity={1} style={{transform: [{translate: [1, 4, 4]}],}} />
+        <SoundShape>
+          <Cylinder
+            radiusTop={0.2}
+            radiusBottom={0.2}
+            dimHeight={0.3}
+            segments={8}
+            lit={true}
+            style={{
+              color: '#96ff00', 
+              transform: [{translate: [-1.5,-0.2,-2]}, {rotateX: 30}],
+            }}
+          />
+        </SoundShape>
       </View>
     );
   }
